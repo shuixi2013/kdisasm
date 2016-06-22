bf2b55203fc2c252e36ba61dc8faa5f5fa2d788c
idhyt <idhyt@hotmail.com>
2016-06-22 21:21:54 +0800

    x

--- config/path.py
+++ config/path.py
@@ -8,10 +8,24 @@
 """
 import os
 
-KALLSYMS_FILE_NAME = "kallsyms-m2-Flyme 5.1.5.0Y"
-KERNEL_FILE_NAME = "kernel-m2-Flyme 5.1.5.0Y"
+KALLSYMS_FILE_NAME = ""
+KERNEL_FILE_NAME = ""
 
 CURRENT_PATH = os.path.split(os.path.realpath(__file__))[0]
 
 KERNEL_FILE_PATH = "/file/".join([CURRENT_PATH, KERNEL_FILE_NAME])
 KALLSYMS_FILE_PATH = "/file/".join([CURRENT_PATH, KALLSYMS_FILE_NAME])
+
+
+if not KALLSYMS_FILE_NAME or not KERNEL_FILE_NAME:
+    file_list = os.listdir("".join([CURRENT_PATH, "/file/"]))
+    for file_ in file_list:
+        if "kernel" in file_:
+            KERNEL_FILE_NAME = file_
+            continue
+        if "kallsyms" in file_:
+            KALLSYMS_FILE_NAME = file_
+            continue
+
+    KERNEL_FILE_PATH = "/file/".join([CURRENT_PATH, KERNEL_FILE_NAME])
+    KALLSYMS_FILE_PATH = "/file/".join([CURRENT_PATH, KALLSYMS_FILE_NAME])
